b7a8004fa28ec1eb79796a8bbde8707ec8569420
BartoszWeb <bartoszdadok@gmail.com>
2022-03-11 16:25:56 +0100

    heroAnim display non

--- components/organisms/HeroAnimation/HeroAnimation.js
+++ components/organisms/HeroAnimation/HeroAnimation.js
@@ -4,6 +4,9 @@ import styled from "styled-components";
 const AnimationWrapper = styled.section`
   max-width: 1480px;
   margin: 0 auto 4em auto;
+  @media (max-width: 800px) {
+    display: none;
+  }
 `;
 import dynamic from 'next/dynamic'
 
