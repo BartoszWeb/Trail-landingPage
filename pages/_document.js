2e5923b6ca7bf2410809134607a4dfa3a2df6c26
BartoszWeb <bartoszdadok@gmail.com>
2022-03-14 22:03:25 +0100

    fonts testing

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
                 <Head>
                     <link rel="shortcut icon" href="/assets/favicon.ico"/>
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
                     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
                           rel="stylesheet"/>
                 </Head>
