d749a9af2fb180928962c5bf38a244aed7620965
BartoszWeb <bartoszdadok@gmail.com>
2022-03-14 13:24:38 +0100

    font and arrow down

--- components/atoms/ArrowDown/ArrowDown.js
+++ components/atoms/ArrowDown/ArrowDown.js
@@ -1,29 +1,12 @@
-import styled from "styled-components";
 import { ImageComponent } from "../ImageComponent/ImageComponent";
+import { ArrowSize } from "./ArrowDown.styles";
 
 const propsLogo = {
     src: "assets/arrow-down.svg",
     alt: "arrow-down",
+    preLoading: true
 };
-const ArrowSize = styled.div`
-  width: 2em;
 
-  &.bounce2 {
-    animation: bounce2 2s ease infinite;
-  }
-
-  @keyframes bounce2 {
-    0%, 20%, 50%, 80%, 100% {
-      transform: translateY(0);
-    }
-    40% {
-      transform: translateY(-4px);
-    }
-    60% {
-      transform: translateY(-2px);
-    }
-  }
-`;
 export const ArrowDown = () => {
     return (
         <ArrowSize className="bounce2">
