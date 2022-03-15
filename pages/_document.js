4531be66c0e9af12fa69aceec4a59b442bb13e00
BartoszWeb <bartoszdadok@gmail.com>
2022-03-15 12:07:04 +0100

    fonts opti

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,41 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+class InlineStylesHead extends Head {
+    getCssLinks() {
+        return this.__getInlineStyles();
+    }
+    
+    __getInlineStyles() {
+        const { assetPrefix, files } = this.context;
+        if (!files || files.length === 0) return null;
+        
+        return files.filter(file => /\.css$/.test(file)).map(file => (
+            <style
+                key={file}
+                data-href={`${assetPrefix}/_next/${file}`}
+                dangerouslySetInnerHTML={{
+                    __html: readFileSync(join(process.cwd(), '.build', file), 'utf-8'),
+                }}
+            />
+        ));
+    }
+}
 
 export default class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
-                <Head>
+                <InlineStylesHead>
                     <link rel="shortcut icon" href="/assets/favicon.ico"/>
                     <link rel="preload" href="/fonts/lato-v22-latin-regular.woff2" as="font" type="font/woff2"
                           crossOrigin=""/>
                     <link rel="preload" href="/fonts/lato-v22-latin-300.woff2" as="font" type="font/woff2"
                           crossOrigin=""/>
-                </Head>
+                    <link rel="stylesheet icon" href="/fonts/style.css"/>
+                </InlineStylesHead>
                 <body>
                 <Main/>
                 <NextScript/>
