6e014b1e3bb52f6d109da161e678f24403569662
BartoszWeb <bartoszdadok@gmail.com>
2022-03-20 18:15:20 +0100

    preloading hero anim

--- components/organisms/HeroAnimation/HeroAnimation.js
+++ components/organisms/HeroAnimation/HeroAnimation.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
 
-
 const AnimationWrapper = styled.section`
   max-width: 1480px;
   margin: 0 auto 4em auto;
@@ -14,13 +13,8 @@ const AnimationWrapper = styled.section`
     display: none;
   }
 `;
+const DynamicHeroAnim = dynamic(() => import("../../atoms/HeroAnimation/HeroAnimation"));
 
-const DynamicComponent = dynamic(() => import("../../atoms/HeroAnimation/HeroAnimation"));
-const FakedImport = dynamic(() => {
-    return import("../../atoms/HeroAnimation/HeroAnimation").then((result) => {
-        return result.default;
-    });
-}, { ssr: false });
 
 export const HeroAnimation = () => {
     const [isDesktop, setIsDesktop] = useState();
@@ -33,7 +27,7 @@ export const HeroAnimation = () => {
     
     return (
         <AnimationWrapper>
-          <DynamicComponent/>
+            { isDesktop && <DynamicHeroAnim/> }
         </AnimationWrapper>
     );
 };
